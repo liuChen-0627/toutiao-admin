e5ce445cbea6f637ace36a87bab8c54319d54ede
liuchen-0627 <wangtianle20@163.com>
2020-12-27 15:33:29 +0800

    项目初始化-调整初始目录

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-27 15:03:46
+ * @LastEditTime: 2020-12-27 15:31:08
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \toutiao-admin\src\main.js
+ */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './style/index.less'
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,17 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-27 15:03:46
+ * @LastEditTime: 2020-12-27 15:26:25
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \toutiao-admin\src\router\index.js
+ */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
 ]
 
 const router = new VueRouter({
