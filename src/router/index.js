1e86f58a6ffdc42d1bac96cb2d96dbf4195cb46d
柳辰 <8404792+liuchen_627@user.noreply.gitee.com>
2020-12-28 12:22:21 +0800

    初始化目录

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,11 @@
-/*
- * @Author: your name
- * @Date: 2020-12-28 11:58:39
- * @LastEditTime: 2020-12-28 12:07:43
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \toutiao-publish-admin\src\router\index.js
- */
+
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import '../style/index.less'
 
 Vue.use(VueRouter)
 
-const routes = [
-
-]
+const routes = []
 
 const router = new VueRouter({
   routes
