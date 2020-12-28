f87a106c27598502dcc967fce9060a61fafe4a74
柳辰 <8404792+liuchen_627@user.noreply.gitee.com>
2020-12-28 12:14:42 +0800

    项目初始化-调整初始目录结构

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,19 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-28 11:58:39
+ * @LastEditTime: 2020-12-28 12:07:43
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \toutiao-publish-admin\src\router\index.js
+ */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import '../style/index.less'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+
 ]
 
 const router = new VueRouter({
